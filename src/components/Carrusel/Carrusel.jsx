77ac3deb693e7c0bf3fdb43da86f2fa911657d8e
BtoMJ <bto.mena.17@gmail.com>
2026-09-06 00:38:05 -0600

    Marco de foto responsive

--- src/components/Carrusel/Carrusel.jsx
+++ src/components/Carrusel/Carrusel.jsx
@@ -1,50 +1,15 @@
-import { useState, useEffect, useRef } from "react";
 import corner1 from "../../assets/novios/corner1.png";
 import corner2 from "../../assets/novios/corner2.png";
 import photo1 from "../../assets/novios/foto-7-corregida.jpeg";
 import "./Carrusel.css";
 
-const slides = [
-  <div className="slide-container-1"></div>,
-  <div className="slide-container-2"></div>,
-  <div className="slide-container-3"></div>,
-  <div className="slide-container-4"></div>,
-  <div className="slide-container-5"></div>,
-  <div className="slide-container-6"></div>,
-  <div className="slide-container-7"></div>,
-  <div className="slide-container-8"></div>,
-  <div className="slide-container-9"></div>,
-];
-
 function Carrusel() {
-  const [index, setIndex] = useState(0);
-  const timeoutRef = useRef(null);
-  const delay = 2700;
-
-  const resetTimeout = () => {
-    if (timeoutRef.current) clearTimeout(timeoutRef.current);
-  };
-
-  useEffect(() => {
-    resetTimeout();
-    timeoutRef.current = setTimeout(
-      () => setIndex((prevIndex) => (prevIndex + 1) % slides.length),
-      delay,
-    );
-    return () => resetTimeout();
-  }, [index]);
-
   return (
     <div className="carrusel-container">
-      <img src={corner1} alt="ramo de flores 1" className="flower-corner-1" />
-      <img src={corner2} alt="ramo de flores 2" className="flower-corner-2" />
       <div className="carousel-fade">
+        <img src={corner1} alt="ramo de flores 1" className="flower-corner-1" />
         <img src={photo1} alt="foto novios" />
-        {/* {slides.map((content, i) => (
-          <div key={i} className={`fade-slide ${i === index ? "active" : ""}`}>
-            {content}
-          </div>
-        ))} */}
+        <img src={corner2} alt="ramo de flores 2" className="flower-corner-2" />
       </div>
 
       <p className="cita">
